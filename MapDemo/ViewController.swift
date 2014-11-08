b9cc8a382d6e0fd7b6b19335213dda8a01211620
Galen Long <nlong@smith.edu>
2014-11-08 16:05:06 -0500

    Demo of world's tallest church on MapKit with pin for church.

--- MapDemo/ViewController.swift
+++ MapDemo/ViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // replace these with coordinates from GPS
+        var latitude:CLLocationDegrees = 48.399193
+        var longitude:CLLocationDegrees = 9.993341
+        
+        // defines how "zoomed in" we are
+        var latitudeDelta:CLLocationDegrees = 0.001
+        var longitudeDelta:CLLocationDegrees = 0.001
+        var span:MKCoordinateSpan = MKCoordinateSpanMake(latitudeDelta, longitudeDelta)
+        
+        // defines what rectangle of map we'll show
+        // centered at location, zoomed in to span
+        var location:CLLocationCoordinate2D =  CLLocationCoordinate2DMake(latitude, longitude)
+        var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
+        self.mapView.setRegion(region, animated: true)
+        
+        // make pin on map, add it
+        var annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Title"
+        annotation.subtitle = "Subtitle"
+
+        self.mapView.addAnnotation(annotation)
+        
     }
 
     override func didReceiveMemoryWarning() {
